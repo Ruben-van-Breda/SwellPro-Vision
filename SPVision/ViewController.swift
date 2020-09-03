98525344ae00a20b220de40b3efa7525b293cc2b
RubenVanBreda <re.vanbreda@gmail.com>
2020-09-03 09:39:37 +0200

    Initial Commit

---
+++ SPVision/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SPVision
+//
+//  Created by Ruben van Breda on 2020/09/03.
+//  Copyright © 2020 Ruben van Breda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SPVisionTests/SPVisionTests.swift
@@ -0,0 +1,34 @@
+//
+//  SPVisionTests.swift
+//  SPVisionTests
+//
+//  Created by Ruben van Breda on 2020/09/03.
+//  Copyright © 2020 Ruben van Breda. All rights reserved.
+//
+
+import XCTest
+@testable import SPVision
+
+class SPVisionTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
