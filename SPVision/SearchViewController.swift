7577440a858ca7607738b0b50d8818923f43e06e
RubenVanBreda <re.vanbreda@gmail.com>
2020-09-06 17:46:43 +0200

    Added Client Session, Menu

---
+++ SPVision/SearchViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SearchViewController.swift
+//  SPVision
+//
+//  Created by Ruben van Breda on 2020/09/05.
+//  Copyright © 2020 Ruben van Breda. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
